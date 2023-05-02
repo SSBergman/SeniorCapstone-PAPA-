3a03fe59e4275346733be07a71be7f126de24d61
Ethan Nguyen-Hopkins <Ethanetnh@gmail.com>
2023-05-01 19:02:03 -0700

    visual changes

---
+++ ASP_Test/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_Test.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
